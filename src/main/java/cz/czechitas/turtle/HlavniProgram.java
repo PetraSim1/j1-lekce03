5f1a55c816e6a78bb6284df3da4792b1d2d7ca34
Petra Simanska <simanska.petra@gmail.com>
2023-02-25 15:20:27 +0100

    Ukol Část 1

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -10,32 +10,57 @@ public class HlavniProgram {
     public void start() {
         zofka = new Turtle();
 
+        zofka.setLocation(100,500);
 
-       pulkruh(1, Color.yellow);
+        nakresliRovnostrannyTrojuhelnik(300, Color.green);
 
-       zofka.turnRight(90);
-       zofka.penUp();
-       zofka.move(130);
-       zofka.turnRight(90);
-       zofka.penDown();
+        zofka.setLocation(400,500);
 
-       pulkruh(1.3f, Color.red);
+       nakresliCtverec(150, Color.yellow);
 
-        zofka.turnRight(90);
-        zofka.penUp();
-        zofka.move(170);
-        zofka.turnRight(90);
-        zofka.penDown();
+        zofka.setLocation(700,500);
 
-        pulkruh(1.7f, Color.green);
+        nakresliObdelnik(100, 200, Color.red);
 
-        zofka.turnRight(90);
-        zofka.penUp();
-        zofka.move(210);
-        zofka.turnRight(90);
-        zofka.penDown();
+        zofka.setLocation(1000,500);
 
-        pulkruh(2f, Color.blue);
+        nakresliKruznici(3, Color.blue);
+
+    }
+
+    private void nakresliObdelnik(float delkaStranyA, float delkaStranyB, Color barva) {
+        for (int i = 0; i < 2; i++) {
+            zofka.setPenColor(barva);
+            zofka.move(delkaStranyA);
+            zofka.turnRight(90);
+            zofka.move(delkaStranyB);
+            zofka.turnRight(90);
+        }
+    }
+
+    private void nakresliCtverec(float delkaStrany, Color barva) {
+        for (int i = 0; i < 4; i++) {
+            zofka.setPenColor(barva);
+            zofka.move(delkaStrany);
+            zofka.turnRight(90);
+        }
+    }
+
+    private void nakresliRovnostrannyTrojuhelnik(float delkaStarany, Color barva) {
+        for (int i = 0; i < 3; i++) {
+            zofka.setPenColor(barva);
+            zofka.move(delkaStarany);
+            zofka.turnRight(120);
+        }
+    }
+
+    private void nakresliKruznici(float delkaStrany, Color barva) {
+        for (int i = 0; i < 360; i++) {
+            zofka.setPenColor(barva);
+            zofka.move(delkaStrany);
+            zofka.turnRight(1);
+
+        }
     }
 
     private void pulkruh (float delkaStrany, Color barva) {
