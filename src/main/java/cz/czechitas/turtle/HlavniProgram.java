c4f08fccb31302604d023f115e9b69cc713e9ba0
Petra Simanska <simanska.petra@gmail.com>
2023-02-25 14:27:09 +0100

    Práce v hodině  - barevná duha

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -2,11 +2,49 @@ package cz.czechitas.turtle;
 
 import cz.czechitas.turtle.engine.*;
 
+import java.awt.*;
+
 public class HlavniProgram {
     private Turtle zofka;
 
     public void start() {
         zofka = new Turtle();
+
+
+       pulkruh(1, Color.yellow);
+
+       zofka.turnRight(90);
+       zofka.penUp();
+       zofka.move(130);
+       zofka.turnRight(90);
+       zofka.penDown();
+
+       pulkruh(1.3f, Color.red);
+
+        zofka.turnRight(90);
+        zofka.penUp();
+        zofka.move(170);
+        zofka.turnRight(90);
+        zofka.penDown();
+
+        pulkruh(1.7f, Color.green);
+
+        zofka.turnRight(90);
+        zofka.penUp();
+        zofka.move(210);
+        zofka.turnRight(90);
+        zofka.penDown();
+
+        pulkruh(2f, Color.blue);
+    }
+
+    private void pulkruh (float delkaStrany, Color barva) {
+        zofka.setPenColor(barva);
+        for (int i = 0; i <180; i++) {
+            zofka.move(delkaStrany);
+            zofka.turnRight(1);
+
+        }
     }
 
     public static void main(String[] args) {
